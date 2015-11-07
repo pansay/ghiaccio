6d90edf943a1e442ef1bdff48a11aa057d06b3e6
pansay <jan.krepelka@gmail.com>
2015-11-07 19:45:41 +0100

    actual caching

--- index.js
+++ index.js
@@ -1,39 +1,20 @@
 var express = require('express'),
-    unirest = require('unirest'),
-    fs = require('fs'),
-    mkdirp = require('mkdirp'),
     app = express(),
     router = express.Router(),
-    config = require('./config');
-
-function cacheWrite(path, content) {
-
-    if (!fs.existsSync(config.cacheFolder)) {
-        mkdirp(config.cacheFolder);
-    }
-
-    fs.writeFile(config.cacheFolder + '/' + path, content, function(err) {
-        if(err) {
-            return console.log(err);
-        }
-    });
-
-    console.log('cache saved');
-}; 
-
-function handler(req, res) {
-    return function(response) {
-        cacheWrite('test.json', response.raw_body);
-        res.json({"path": req.params.path, "response": response.body});
-    }
-}
+    config = require('./config'),
+    g = require('./src/g');
 
 router
     .get('/:path(*)', function (req, res) {
-        unirest
-            .get(config.gitHubUrl + req.params.path)
-                .headers({'User-Agent': config.userAgent}) // https://developer.github.com/v3/#user-agent-required
-                .end(handler(req, res));
+        var path = req.params.path;
+
+        if (g.isCached(path)) {
+            g.getCacheAndOutput(res, path);
+        }
+        else {
+            g.writeCacheAndOutput(res, path);
+        }
+        
     });
 
 app
---
+++ src/g.js
@@ -0,0 +1,57 @@
+var unirest = require('unirest'),
+    fs = require('fs'),
+    mkdirp = require('mkdirp'),
+    config = require('../config');
+
+function getPath(path) {
+    return config.cacheFolder + '/' + path.replace(/\//g, '_') + '.json';
+}
+
+function cacheWrite(path, content) {
+
+    if (!fs.existsSync(config.cacheFolder)) {
+        mkdirp(config.cacheFolder);
+    }
+
+    fs.writeFile(path, content, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    });
+
+}
+
+function ghHandler(res, path) {
+    return function(response) {
+        cacheWrite(path, response.raw_body);
+        output(res, response.body);
+    };
+}
+
+function cacheHandler(res) {
+    return function (err, content) {
+        if (err) {
+            throw err;
+        }
+        output(res, JSON.parse(content));
+    };
+}
+
+function output(res, content) {
+    res.json(content);
+}
+
+module.exports =  {
+    isCached: function(path) {
+        return fs.existsSync(getPath(path));
+    },
+    getCacheAndOutput: function(res, path) {
+        fs.readFile(getPath(path), cacheHandler(res));
+    },
+    writeCacheAndOutput: function(res, path) {
+        unirest.get(config.gitHubUrl + path)
+            .headers({'User-Agent': config.userAgent}) // https://developer.github.com/v3/#user-agent-required
+            .end(ghHandler(res, getPath(path)));
+    }
+};
+

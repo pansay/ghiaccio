38611547a398359a998bc2dbf1168127a521d2be
pansay <jan.krepelka@gmail.com>
2015-11-07 18:13:53 +0100

    basic cache save

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 var express = require('express'),
-	unirest = require('unirest'),
+    unirest = require('unirest'),
+    fs = require('fs'),
+    mkdirp = require('mkdirp'),
     app = express(),
     router = express.Router(),
     config = require('./config');
 
+function cacheWrite(path, content) {
+
+    if (!fs.existsSync(config.cacheFolder)) {
+        mkdirp(config.cacheFolder);
+    }
+
+    fs.writeFile(config.cacheFolder + '/' + path, content, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    });
+
+    console.log('cache saved');
+}; 
+
 function handler(req, res) {
-	return function(response) {
-		res.json({"path": req.params.path, "response": response.body});
-	}
+    return function(response) {
+        cacheWrite('test.json', response.raw_body);
+        res.json({"path": req.params.path, "response": response.body});
+    }
 }
 
 router
-	.get('/:path(*)', function (req, res) {
-	    unirest
-	    	.get(config.gitHubUrl + req.params.path)
-		    	.headers({'User-Agent': config.userAgent}) // https://developer.github.com/v3/#user-agent-required
-		    	.end(handler(req, res));
-	});
+    .get('/:path(*)', function (req, res) {
+        unirest
+            .get(config.gitHubUrl + req.params.path)
+                .headers({'User-Agent': config.userAgent}) // https://developer.github.com/v3/#user-agent-required
+                .end(handler(req, res));
+    });
 
 app
     .use('/', router)
